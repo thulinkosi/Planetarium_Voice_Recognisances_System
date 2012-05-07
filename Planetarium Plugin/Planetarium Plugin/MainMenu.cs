0387dca9b513c1020cbd0147696f71a8159859c6
Thuli Nkosi <thulihnkosi@yahoo.com>
2012-05-07 10:27:34 -0700

    testing

--- Planetarium Plugin/Planetarium Plugin/MainMenu.cs
+++ Planetarium Plugin/Planetarium Plugin/MainMenu.cs
@@ -43,9 +43,10 @@ namespace Planetarium_Plugin
         }
 
         //slide change function
+
         void Application_SlideSelectionChanged(PowerPoint.SlideRange SldRange) 
         {
-            try
+          /**  try
            {
                 if (SldRange != null )
                 {
@@ -60,7 +61,7 @@ namespace Planetarium_Plugin
            catch (Exception ex) 
            {
 
-            }
+            } */
         }
 
         private void cmdStart_Click(object sender, RibbonControlEventArgs e)
